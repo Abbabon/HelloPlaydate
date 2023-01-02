13eb0152f2c697e3b76e745e4d7a56f759497787
Amit Netanel <netanel.amit@gmail.com>
2023-01-02 21:47:35 +0000

    game now has rudimentary sprites and movement

--- source/main.lua
+++ source/main.lua
@@ -1,3 +1,45 @@
+import "CoreLibs/object"
+import "CoreLibs/graphics"
+import "CoreLibs/sprites"
+import "CoreLibs/timer"
+--import "CoreLibs/crank"
+
+local gfx <const> = playdate.graphics
+local playerSprite = nil
+local playerSpeed = 4
+
+local function initialize()
+	local playerImage = gfx.image.new("images/PDRobo")
+	playerSprite = gfx.sprite.new(playerImage)
+	playerSprite:moveTo(200,120)
+	playerSprite:add()
+	
+	local backgroundImage = gfx.image.new("images/background")
+	gfx.sprite.setBackgroundDrawingCallback(
+		function(x, y, width, height)
+			gfx.setClipRect(x, y, width, height)
+			backgroundImage:draw(0, 0)
+			gfx.clearClipRect()
+		end
+		)
+end
+
+initialize()
+
 function playdate.update()
-	print("LUA DEVELOPER")
+	if (playdate.buttonIsPressed(playdate.kButtonUp)) then
+		playerSprite:moveBy(0, -playerSpeed)
+	end
+	if (playdate.buttonIsPressed(playdate.kButtonDown)) then
+		playerSprite:moveBy(0, playerSpeed)
+	end
+	if (playdate.buttonIsPressed(playdate.kButtonLeft)) then
+		playerSprite:moveBy(-playerSpeed, 0)
+	end
+	if (playdate.buttonIsPressed(playdate.kButtonRight)) then
+		playerSprite:moveBy(playerSpeed, 0)
+	end
+	
+	playdate.timer.updateTimers()
+	gfx.sprite.update()
 end
